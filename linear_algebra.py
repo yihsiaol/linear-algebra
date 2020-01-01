836337d46af93986ed2e7e2e1588fe1e2809ed6c
Yi-Hsiao Liu <yliu@evolvevacationrental.com>
2020-01-01 11:04:06 -0700

    commenting methods

--- linear_algebra.py
+++ linear_algebra.py
@@ -14,18 +14,23 @@ class Vector:
     def __str__(self):
         return 'Vector{}'.format(self.components)
 
+    # add vectors
     def __add__(self, other):
         return Vector(*map(sum, zip(self.components, other.components)))
 
+    # subtract vectors
     def __sub__(self, other):
         return Vector(*map(lambda x: x[0] - x[1], zip(self.components, other.components)))
 
+    # scalar multiplication
     def __mul__(self, other):
         return Vector(*map(lambda x: x * other, self.components))
 
+    # find magnitude of the vector instance
     def magnitude(self):
         return sqrt(sum(map(lambda x: x**2, self.components)))
 
+    # normalize the vector instance to a unit vector to find its direction
     def normalize(self):
         return self * (1 / self.magnitude())
 
